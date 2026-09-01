68343325ebb4ee7e5f1324304a515fc235d2109a
nacer-chouich <nacreddinechouich0012@gmail.com>
2026-09-01 08:37:17 +0100

    quick fix

--- infra/discovery-server/src/main/java/com/softpos/discovery_server/DiscoveryServerApplication.java
+++ infra/discovery-server/src/main/java/com/softpos/discovery_server/DiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package com.softpos.discovery_serve;
+package com.softpos.discovery_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class DiscoveryServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryServeApplication.class, args);
+        SpringApplication.run(DiscoveryServerApplication.class, args);
     }
 
 }
